15d5e8285fc443fd93ae47fb8771d7a6b94b223b
Vinicius Costa <vinicosta@usp.br>
2020-11-18 23:37:52 -0300

    Pt.3 do tutorial

--- eventos/urls.py
+++ eventos/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'eventos'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<str:evento_nome>/', views.detail, name='detail'),
+    path('<str:evento_nome>/inscricao/', views.inscricao, name='inscricao'),
 ]
--- eventos/views.py
+++ eventos/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Evento
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Eu sou um evento!")
+    lista_evn_rct = Evento.objects.order_by('-inicio')[:5]
+    context = {'lista_evn_rct':lista_evn_rct}
+    return render(request, 'eventos/index.html', context)
 
+
+def detail(request, evento_nome):
+    evento = get_object_or_404(Evento, nome=evento_nome)
+    return render(request, 'eventos/detail.html', {'evento': evento})
+
+
+def inscricao(request, evento_nome):
+    return HttpResponse('Você está se inscrevendo no evento %s' %evento_nome)
